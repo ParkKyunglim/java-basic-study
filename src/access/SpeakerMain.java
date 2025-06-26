5c7de839d9106e2c55551c5368ef802087592bc2
ParkKyunglim <pkl1004125@gmail.com>
2025-06-26 19:56:41 +0900

    [섹션7] 41. 접근 제어자 이해1

---
+++ src/access/SpeakerMain.java
@@ -0,0 +1,20 @@
+package access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();;
+
+        speaker.volumeUp();
+        speaker.showVolume();;
+
+        // 필드에 직접 접근 - 원하는 값 변경
+        // 외부 접근을 막아야 함
+        System.out.println("volume 필드 직접 접근 수정");
+        speaker.volume = 200;
+        speaker.showVolume();
+    }
+}
