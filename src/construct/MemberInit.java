8c363717dff03404dc9ff075131c87d2cff33881
ParkKyunglim <pkl1004125@gmail.com>
2025-06-26 17:37:11 +0900

    [섹션5] 30. this

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -4,4 +4,11 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    // this. 인스턴스 자신의 참조값을 뜻함
+    void initMember(String name, int age, int grade) {
+        this.name = name; // x001.name = "user";
+        this.age = age;
+        this.grade = grade;
+    }
 }
