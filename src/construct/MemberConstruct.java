28621e7ba70b75c33788127f07bbb1ea2331f0f5
ParkKyunglim <pkl1004125@gmail.com>
2025-06-26 19:00:53 +0900

    [섹션5] 33. 생성자 - 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,12 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    // this() 생성자 내부에서 자신의 생성자 호출 - 중복 제거
+    // 첫 줄에서만 사용 가능
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); // 변경
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
         this.name = name;
