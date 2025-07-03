c532197b29cbe2434c0d86405cbbabe4dec7f95c
ParkKyunglim <pkl1004125@gmail.com>
2025-07-03 17:34:12 +0900

    [섹션7] 45. 접근 제어자 사용 - 클레스 레벨(PublicClass 추가)

---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,15 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefalutClass1 class1 = new DefalutClass1();
+        DefalutClass2 class2 = new DefalutClass2();
+    }
+}
+
+class DefalutClass1 {
+}
+
+class DefalutClass2 {
+}
